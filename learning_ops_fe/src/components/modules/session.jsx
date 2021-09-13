570b223adcf49d197172694c5b1d4ee04a38bc72
Chris Palmer <chris.palmer@and.digital>
2021-09-13 09:41:18 +0100

    UseEffect now in place and looping through real data.
    Signed-off-by: Chris Palmer <chris.palmer@and.digital>

--- learning_ops_fe/src/components/modules/session.jsx
+++ learning_ops_fe/src/components/modules/session.jsx
@@ -1,43 +1,33 @@
 import { Title } from "../atoms/title";
 import { Paragraph } from "../atoms/paragraph";
+import { useEffect, useState } from 'react';
 const axios = require('axios');
 
-const sessions = [{
-    "title": "Basic Wireframing",
-    "description": "Basic Wireframing Description"
-  },
-  {
-    "title": "Basic Protoyping",
-    "description": "Basic Prototyping Description"
-  },
-  ]
-
-
-
-  
-
 export const Session = () => {
-    async function getSession() {
-        try {
-          const response = await axios.get('http://127.0.0.1:8000/sessions/');
-          console.log(response);
-          return response;
-        } catch (error) {
-          console.error(error);
-        }
+
+  const [sessions, setSessions] = useState();
+
+  useEffect(() => {
+    const getSessions = async () => {
+      try {
+        const res = await axios.get('http://127.0.0.1:8000/sessions');
+        setSessions(res.data)
+      } catch(e){
+        console.log(e);
       }
-      //  do I need to call this? look at the other await functions first.
-      
-    const data = getSession();
-    console.log(data);
-    // this fails because it tries to run an empty array. 
-    const allSessions = sessions.map((data, index) => {
+    }; getSessions();
+  }, [])
+ 
+  
         return(
-            <div key={index}>
-            <Title text={data.title}/>
-            <Paragraph text={data.description} />
-            </div>
+          <>
+          {sessions && sessions.map((session, index) => {
+            return(
+              <div key={index}>
+              <Title text={session.name}/>
+              <Paragraph text={session.description} />
+              </div>
             )
-        })
-    return allSessions;
+          })}
+          </>) 
 }    ;
